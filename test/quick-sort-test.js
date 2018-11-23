311ebe28fb5d63cc3c52b194d97492f84820a57a
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-23 13:07:31 -0700

    adds test for total quick sort algorithm

---
+++ test/quick-sort-test.js
@@ -0,0 +1,12 @@
+assert = require('chai').assert
+const quickSort = require('../quick-sort')
+
+describe("Sorting Algorithm", function() {
+  context("Quick Sort", function() {
+    it('should sort the array', function () {
+      const originalArray = [8,4,5,9,1]
+      const sortedArray - [1,4,5,8,9]
+      assert.deepEqual(quickSort(originalArray), sortedArray)
+    });
+  });
+});
