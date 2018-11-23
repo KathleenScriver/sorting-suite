2e4078c4bdeedcdb711fa23609f71564f25ba149
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-23 13:14:25 -0700

    Adds node modules and fixes test

--- test/quick-sort-test.js
+++ test/quick-sort-test.js
@@ -4,8 +4,8 @@ const quickSort = require('../quick-sort')
 describe("Sorting Algorithm", function() {
   context("Quick Sort", function() {
     it('should sort the array', function () {
-      const originalArray = [8,4,5,9,1]
-      const sortedArray - [1,4,5,8,9]
+      const originalArray = [8,4,5,9,1];
+      const sortedArray = [1,4,5,8,9];
       assert.deepEqual(quickSort(originalArray), sortedArray)
     });
   });
